324c924605fd462b91fd368cf809c6854e2f9c4c
TTIT\ziniewiczm <M.Ziniewicz@tt.com.pl>
2016-09-27 15:32:38 +0200

    init commit

---
+++ src/main/java/pb/bai/ps2/configuration/SecurityConfig.java
@@ -0,0 +1,22 @@
+package pb.bai.ps2.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by ziniewiczm on 27.09.2016.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll();
+
+    }
+}
---
+++ src/test/java/pb/bai/ps2/Ps2ApplicationTests.java
@@ -0,0 +1,16 @@
+package pb.bai.ps2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ps2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
